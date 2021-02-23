8ccfbf409cc286db94ba63a5ccf19165e9aebe14
scord <70288480+scordd@users.noreply.github.com>
2021-02-23 12:37:20 -0800

    First commit

---
+++ Script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: ascii -*-
+# -*- coding: latin-1 -*-
+# -*- coding: utf-42 -*-
+
+print("""
+ _                     __  __   ______   _____    ____     _____   ______
+| |          /\       |  \/  | |  ____| |_   _|  / __ \   / ____| |  ____|
+| |         /  \      | \  / | | |__      | |   | |  | | | (___   | |__
+| |        / /\ \     | |\/| | |  __|     | |   | |  | |  \___ \  |  __|
+| |____   / ____ \    | |  | | | |____   _| |_  | |__| |  ____) | | |____
+|______| /_/    \_\   |_|  |_| |______| |_____|  \____/  |_____/  |______|
+""")
+print("Cree par Jacques Blasset et Sava Rozsnyai - Sciences 9C")
+txt = raw_input("Type something to test this out: ")
+print "Is this what you just said?", txt
