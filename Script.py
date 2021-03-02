6fd15388f5c5463823dd0035952e3f23084cdac8
scord <70288480+scordd@users.noreply.github.com>
2021-03-01 18:12:34 -0800

    wd

--- Script.py
+++ Script.py
@@ -6,7 +6,7 @@
 import time
 import sys
 
-score = 0;
+score = 0
 
 print("""
  _                     __  __   ______   _____    ____     _____   ______
@@ -22,7 +22,7 @@ time.sleep(2)
 print("""
 
 1e question""")
-q1 = raw_input("""
+q1 = input("""
 La troisieme phase de la meiose s'appelle:
 a) la Metaphase
 b) la Prophase
@@ -33,7 +33,7 @@ if q1 == "c":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
@@ -41,7 +41,7 @@ time.sleep(2)
 print("""
 
 2e question""")
-q2 = raw_input("""
+q2 = input("""
 A la fin Metaphase I, les chromosomes sont alignes a l'Equateur.
 a) vrai
 b) faux
@@ -50,7 +50,7 @@ if q2 == "a":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
@@ -58,7 +58,7 @@ time.sleep(2)
 print("""
 
 3e question""")
-q3 = raw_input("""
+q3 = input("""
 Dans l'etape de Telophase I, les cellules filles sont:
 a) diploides
 b) hiploides
@@ -68,7 +68,7 @@ if q3 == "c":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
@@ -76,7 +76,7 @@ time.sleep(2)
 print("""
 
 4e question""")
-q4 = raw_input("""
+q4 = input("""
 Place dans le bon ordre:
 a) Telophase - Prophase - Metaphase - Anaphase
 b) Prophase - Metaphase - Anaphase - Telophase
@@ -87,7 +87,7 @@ if q4 == "b":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
@@ -95,7 +95,7 @@ time.sleep(2)
 print("""
 
 5e question""")
-q5 = raw_input("""
+q5 = input("""
 Apres la fin du Telophase II, combien de cellules filles est-ce qu'il y a?
 a) 8
 b) 69
@@ -106,7 +106,7 @@ if q5 == "d":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
@@ -114,7 +114,7 @@ time.sleep(2)
 print("""
 
 6e question""")
-q6 = raw_input("""
+q6 = input("""
 Il y a deux interphases.
 a) vrai
 b) faux
@@ -123,7 +123,7 @@ if q6 == "b":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
@@ -131,7 +131,7 @@ time.sleep(2)
 print("""
 
 7e question""")
-q7 = raw_input("""
+q7 = input("""
 La prophase I prend 90% de la duree totale de la meiose.
 a) vrai
 b) faux
@@ -140,7 +140,7 @@ if q7 == "a":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
@@ -148,7 +148,7 @@ time.sleep(2)
 print("""
 
 8e question""")
-q8 = raw_input("""
+q8 = input("""
 Comment peut-on decrire la meiose ?
    a) une division rationnelle
    b) une division equationnelle
@@ -158,71 +158,71 @@ if q8 == "b" or "c":
   print("Bonne reponse!")
   score = score + 1
   print("Score:" + str(score))
-else:
+  else:
   print("Mauvaise reponse!")
   print("Score:" + str(score))
 
-  time.sleep(2)
-  print("""
+time.sleep(2)
+print("""
 
-  9e question""")
-  q9 = raw_input("""
-  Dans quel etape se produit les haploides de chromosomes repliques?
-   a) Prophase 1
-   b) Telophase 1
-   c) Anaphase 2
-  """)
-  if q9 == "b":
-    print("Bonne reponse!")
-    score = score + 1
-    print("Score:" + str(score))
+9e question""")
+q9 = input("""
+Dans quel etape se produit les haploides de chromosomes repliques?
+a) Prophase 1
+b) Telophase 1
+c) Anaphase 2
+""")
+if q9 == "b":
+  print("Bonne reponse!")
+  score = score + 1
+  print("Score:" + str(score))
   else:
-    print("Mauvaise reponse!")
-    print("Score:" + str(score))
+  print("Mauvaise reponse!")
+  print("Score:" + str(score))
 
-  time.sleep(2)
-  print("""
+time.sleep(2)
+print("""
 
-  10e question""")
-  q10 = raw_input("""
-  Qu'est-ce qu'une cellule diploide?
-   a) 2n chromosomes
-   b) n chromosomes
-   c) 4n chromosomes
-   d) 3n chromosomes
-  """)
-  if q10 == "a":
-    print("Bonne reponse!")
-    score = score + 1
-    print("Score:" + str(score))
+10e question""")
+q10 = input("""
+Qu'est-ce qu'une cellule diploide?
+a) 2n chromosomes
+b) n chromosomes
+c) 4n chromosomes
+d) 3n chromosomes
+""")
+if q10 == "a":
+  print("Bonne reponse!")
+  score = score + 1
+  print("Score:" + str(score))
   else:
-    print("Mauvaise reponse!")
-    print("Score:" + str(score))
+  print("Mauvaise reponse!")
+  print("Score:" + str(score))
 
- time.sleep(2)
- print("""
+time.sleep(2)
+print("""
 
- 11e question""")
- q11 = raw_input("""
- Qu'est-ce qu'une cellule haploide?
-  a) 2n chromosomes a 1 chromatide
-  b) n chromosomes
-  c) 4n chromosomes
-  d) 3n chromosomes
- """)
- if q11 == "b":
-   print("Bonne reponse!")
-   score = score + 1
-   print("Score:" + str(score))
- else:
-   print("Mauvaise reponse!")
+11e question""")
+q11 = input("""
+Qu'est-ce qu'une cellule haploide?
+a) 2n chromosomes a 1 chromatide
+b) n chromosomes
+c) 4n chromosomes
+d) 3n chromosomes
+""")
+if q11 == "b":
+print("Bonne reponse!")
+score = score + 1
+print("Score:" + str(score))
+else:
+print("Mauvaise reponse!")
    print("Score:" + str(score))
 
  time.sleep(2)
  print("""
 
  12e question""")
- q12 = raw_input("""
+ q12 = input("""
  Quelle est la definition de la meiose?
   a) La succession de deux replications precedees d'une unique division
   b) Permet obtention de gametes haploides
@@ -241,7 +241,7 @@ else:
  print("""
 
  13e question""")
- q13 = raw_input("""
+ q13 = input("""
  Qu'est ce qu'on obtient apres la premiere division de la meiose?
   a) 2 cellules a n chromosomes a deux chromatides
   b) 4 cellules a 2n chromosomes a une chromatide
@@ -260,7 +260,7 @@ else:
  print("""
 
  14e question""")
- q14 = raw_input("""
+ q14 = input("""
  Le noyau d'un ovule humain renferme ...
  a) 23 chromosomes dont un chromosome X.
  b) 23 chromosomes dont un chromosome X ou Y.
@@ -278,7 +278,7 @@ else:
  print("""
 
  15e question""")
- q15 = raw_input("""
+ q15 = input("""
  Le noyau des gamètes humains possedent ...
  a) 46 chromosomes.
  b) 23 chromosomes dont un chromosome sexuel.
@@ -296,7 +296,7 @@ else:
  print("""
 
  16e question""")
- q16 = raw_input("""
+ q16 = input("""
  Le sexe d'un enfant a naitre est determine par ...
  a) Le chromosome sexuel contenu dans le spermatozoide.
  b) Le chromosome sexuel contenu dans l'ovule.
@@ -314,7 +314,7 @@ time.sleep(2)
 print("""
 
 17e question""")
-q17 = raw_input("""
+q17 = input("""
 Le noyau d'un spermatozoide humain renferme ...
 a) 23 chromosomes dont un chromosome X.
 b) 23 chromosomes dont un chromosome X ou Y.
